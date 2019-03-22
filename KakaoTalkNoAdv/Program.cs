382bada20e64e116757219d84c7ca5230b7396a8
horyu1234 <horyu1234@naver.com>
2019-03-22 22:19:34 +0900

    버전을 1.1.0 으로 업그레이드
    광고 제거 시 별도의 창을 띄우지 않고, 결과만 우측 하단 알림으로 보여주도록 수정

--- KakaoTalkNoAdv/Program.cs
+++ KakaoTalkNoAdv/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Windows.Forms;
 using Microsoft.Win32;
 
 namespace KakaoTalkNoAdv
@@ -41,69 +42,53 @@ namespace KakaoTalkNoAdv
 
         private static void Main(string[] args)
         {
-            Console.Title = "카카오톡 PC 버전 광고 제거 v1.0.0 [By horyu1234]";
-
-            Console.BackgroundColor = ConsoleColor.DarkBlue;
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Clear();
-
-            Console.WriteLine("프로그램 개발자: horyu1234");
-            Console.WriteLine();
-            Console.WriteLine("본 프로그램의 소스는 https://github.com/horyu1234/KakaoTalkNoAdv 에서 공개하고 있습니다.");
-            Console.WriteLine();
-            Console.WriteLine("본 프로그램은 real21c 님의 kakao-noadv 프로젝트의 구조를 참고하여 개발되었습니다.");
-            Console.WriteLine();
-            Console.WriteLine("==============================");
-            Console.WriteLine();
-
             try
             {
                 CheckKakaoTalkIsRunningOrStart();
 
-                Console.WriteLine("카카오톡 PC 버전의 창을 찾는 중입니다.");
                 var kakaoTalkWindowId = GetKakaoTalkWindowId();
 
                 if (kakaoTalkWindowId == 0)
                 {
-                    Console.WriteLine("카카오톡 PC 버전의 창을 찾을 수 없습니다.");
-                    Shutdown();
+                    ShowBalloonTip("카카오톡 PC 버전의 창을 찾을 수 없습니다.", 3000);
+                    QuitProgram(5000);
                 }
 
                 RemoveAds(kakaoTalkWindowId);
+
+                ShowBalloonTip("광고가 제거되었습니다.", 2000);
             }
             catch (Exception e)
             {
-                Console.WriteLine("작업을 진행하는 중 오류가 발생하였습니다.");
-                Console.WriteLine(e.Message);
+                ShowBalloonTip("작업을 진행하는 중 오류가 발생하였습니다.\n" +
+                               e.Message, 3000);
             }
 
-            Shutdown();
+            QuitProgram(5000);
+
+            Console.ReadLine();
         }
 
         private static void CheckKakaoTalkIsRunningOrStart()
         {
-            Console.WriteLine("카카오톡 PC 버전이 실행 중인지 확인하는 중입니다.");
-
             var kakaoTalkProcess = Process.GetProcessesByName("kakaotalk");
-            if (kakaoTalkProcess.Length == 0)
+            if (kakaoTalkProcess.Length != 0)
             {
-                Console.WriteLine("카카오톡 PC 버전이 실행 중이 아닙니다.");
-
-                Console.WriteLine("카카오톡 PC 버전을 실행합니다.");
-                var classesRoot = Registry.ClassesRoot;
-                var subKey = classesRoot.OpenSubKey("kakaoopen\\shell\\open\\command");
-
-                var kakaoTalkStartCommand = subKey.GetValue(null).ToString();
-                var kakaoTalkExePath = Regex.Split(Regex.Split(kakaoTalkStartCommand, "\" \"%1\"")[0], "\"")[1];
-
-                Process.Start(kakaoTalkExePath);
-
-                WaitWhileKakaoTalkRunning();
+                return;
             }
-            else
-            {
-                Console.WriteLine("카카오톡 PC 버전이 실행 중입니다.");
-            }
+
+            ShowBalloonTip("카카오톡 PC 버전이 실행 중이 아닙니다.\n" +
+                           "카카오톡 PC 버전을 실행합니다.", 5000);
+
+            var classesRoot = Registry.ClassesRoot;
+            var subKey = classesRoot.OpenSubKey("kakaoopen\\shell\\open\\command");
+
+            var kakaoTalkStartCommand = subKey.GetValue(null).ToString();
+            var kakaoTalkExePath = Regex.Split(Regex.Split(kakaoTalkStartCommand, "\" \"%1\"")[0], "\"")[1];
+
+            Process.Start(kakaoTalkExePath);
+
+            WaitWhileKakaoTalkRunning();
         }
 
         private static int GetKakaoTalkWindowId()
@@ -127,8 +112,6 @@ namespace KakaoTalkNoAdv
 
         private static void RemoveAds(int kakaoTalkWindowId)
         {
-            Console.WriteLine("광고를 제거하는 중입니다.");
-
             var friendList = FindWindowEx(kakaoTalkWindowId, 0, "EVA_ChildWindow", null);
             var evaWindow = FindWindowEx(kakaoTalkWindowId, 0, "EVA_Window", null);
 
@@ -140,37 +123,53 @@ namespace KakaoTalkNoAdv
             SetWindowPos(new IntPtr(friendList), HWND_BOTTOM, 0, 0,
                 kakaoTalkWindowRect.Right - kakaoTalkWindowRect.Left,
                 kakaoTalkWindowRect.Bottom - kakaoTalkWindowRect.Top - 36, SWP_NOMOVE);
-
-            Console.WriteLine("광고가 제거되었습니다.");
-        }
-
-        private static void Shutdown()
-        {
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            for (var remainSeconds = 5; remainSeconds > 0; remainSeconds--)
-            {
-                Console.WriteLine(remainSeconds + " 초 후 프로그램이 종료됩니다.");
-                Thread.Sleep(1000);
-            }
-
-            Process.GetCurrentProcess().Kill();
         }
 
         private static void WaitWhileKakaoTalkRunning()
         {
+            int waitSeconds = 0;
             while (Process.GetProcessesByName("kakaotalk").Length == 0)
             {
-                Console.Write(".");
+                waitSeconds++;
+
+                if (waitSeconds > 60)
+                {
+                    ShowBalloonTip("카카오톡이 실행 여부를 확인하지 못하였습니다.", 3000);
+                    QuitProgram(5000);
+                    break;
+                }
+
                 Thread.Sleep(1000);
             }
 
-            for (var remainSeconds = 10; remainSeconds > 0; remainSeconds--)
+            Thread.Sleep(10000);
+        }
+
+        private static void ShowBalloonTip(string message, int timeout)
+        {
+            new Thread((() =>
             {
-                Console.WriteLine(remainSeconds + " 초 후 작업을 시작합니다.");
-                Thread.Sleep(1000);
-            }
+                var notification = new NotifyIcon
+                {
+                    Visible = true,
+                    Icon = System.Drawing.SystemIcons.Information,
+                    BalloonTipTitle = "KakaoTalkNoAdv v1.1.0 (개발자: horyu1234)",
+                    BalloonTipText = message
+                };
+
+                notification.ShowBalloonTip(timeout);
+
+                Thread.Sleep(timeout);
+
+                notification.Dispose();
+            })).Start();
+        }
+
+        private static void QuitProgram(int timeout)
+        {
+            Thread.Sleep(timeout);
+
+            Process.GetCurrentProcess().Kill();
         }
     }
 }
